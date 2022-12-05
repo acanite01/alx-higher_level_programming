5ed5e94657e43d5c99c7b325cb9fff79130dd213
acanite01 <107746972+acanite01@users.noreply.github.com>
2022-12-05 02:03:01 +0100

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+count = 1
+for w in range(0, 9):
+    for i in range(count, 10):
+        if w == 8 and i == 9:
+            print("{}".format(89))
+            continue
+        print("{}{}, ".format(w, i), end="")
+    count = count + 1
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
