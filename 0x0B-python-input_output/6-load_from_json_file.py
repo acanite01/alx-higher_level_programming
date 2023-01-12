a7104cef8b57d6e741eef0efb8dad7f49d84b959
acanite01 <107746972+acanite01@users.noreply.github.com>
2023-01-12 14:50:23 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -13,4 +13,4 @@ def load_from_json_file(filename):
         filename: textfile name
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.load(f):wq
+        return json.load(f)
